f16ed82221cb2ec575956b251e63261a39bd3ea4
morozec <andre15@inbox.ru>
2020-05-23 12:42:15 +0400

    Comparison settings table

--- ClientApp/src/components/Comparing.js
+++ ClientApp/src/components/Comparing.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react'
-import { Table, Button } from 'react-bootstrap';
+import React, { Fragment, useState, useEffect } from 'react'
+import { Table, Button, Modal } from 'react-bootstrap';
 import { RatioHelper, GROUPS } from './../RatiosHelper';
 import { Link } from 'react-router-dom';
 
@@ -7,6 +7,29 @@ export function Comparing(props) {
     const { comparingCompanies, removeComparingCompany } = props;
     const groupedRatios = comparingCompanies.map(c => (new RatioHelper(c.ratios)).getGroupedRatios());
     const EPS = 1E-3;
+
+    const [visibility, setVisibility] = useState({});
+    const [visibilityTmp, setVisibilityTmp] = useState({});
+    const [showSettings, setShowSettings] = useState(false);
+
+    const recommendationsNames = ['Consensus', 'Average Price Target', 'Upside to Average Price Target'];
+
+    useEffect(() => {
+        let startVisibility = {};
+        for (let groupName of Object.keys(GROUPS)) {
+            startVisibility[groupName] = true;
+            for (let ratioName of GROUPS[groupName]) {
+                startVisibility[ratioName] = true;
+            }
+        }
+        startVisibility.Recommendations = true;
+        for (let rn of recommendationsNames){
+            startVisibility[rn] = true;
+        }
+
+        setVisibility(startVisibility);
+        setVisibilityTmp(startVisibility);
+    }, [])
 
     const getRatioClass = (companyRatio, allCompaniesValues) => {
         const min = Math.min(...allCompaniesValues);
@@ -92,8 +115,55 @@ export function Comparing(props) {
         removeComparingCompany(companySimId);
     }
 
+    const handleShowSettings = () => {
+        setVisibilityTmp(visibility);
+        setShowSettings(true);
+    }
+    const handleHideSettings = () => {
+        setShowSettings(false);
+    }
+
+    const isGroup = (name) => name === 'Recommendations' || GROUPS.hasOwnProperty(name);
+
+    const handleVisibilityChanged = (name, e) => {
+        let vt = { ...visibilityTmp};
+        vt[name] = e.target.checked;
+        if (GROUPS.hasOwnProperty(name)){
+            for (let ratioName of GROUPS[name]){
+                vt[ratioName] = e.target.checked;
+            }
+        }else if (name === 'Recommendations'){
+            for (let rn of recommendationsNames){
+                vt[rn] = e.target.checked;
+            }
+        }else{
+            if (!recommendationsNames.some(rn => rn === name)){
+                let groupName = Object.keys(GROUPS).filter(g => GROUPS[g].some(rn => rn === name))[0];
+                if (GROUPS[groupName].every(rn => vt[rn] === e.target.checked)){
+                    vt[groupName] = e.target.checked;
+                }
+            }else{
+                if (recommendationsNames.every(rn => vt[rn] === e.target.checked)){
+                    vt['Recommendations'] = e.target.checked;
+                }
+            }
+        }
+        setVisibilityTmp(vt);
+    }
+
+    const saveSetting = () => {
+        setVisibility(visibilityTmp);
+        setShowSettings(false);
+    }
+
+  
+
     return (
         <div>
+            <div className='d-flex'>
+                <Button variant='outline-primary' className='ml-auto' onClick={handleShowSettings}>Settings</Button>
+            </div>
+
             <Table bordered hover variant='light' className='table-sm'>
                 <thead>
                     <tr>
@@ -117,12 +187,12 @@ export function Comparing(props) {
                 </thead>
                 <tbody>
 
-                    {Object.keys(GROUPS).map(groupName =>
+                    {Object.keys(GROUPS).filter(groupName => visibility[groupName]).map(groupName =>
                         <Fragment key={groupName}>
                             <tr className='groupName'>
                                 <th colSpan={comparingCompanies.length + 1} className='centered'>{groupName}</th>
                             </tr>
-                            {GROUPS[groupName].map((ratioName, i) =>
+                            {GROUPS[groupName].filter(ratioName => visibility[ratioName]).map((ratioName, i) =>
                                 <tr key={i}>
                                     <td>
                                         {ratioName}
@@ -136,33 +206,90 @@ export function Comparing(props) {
                         </Fragment>
                     )}
 
-                    <tr className='groupName'>
-                        <th colSpan={comparingCompanies.length + 1} className='centered'>Recommendations</th>
-                    </tr>
-                    <tr>
-                        <td>Consensus</td>
-                        {comparingCompanies.map((c, i) => <td key={i}
-                            className={getNumberClass(
-                                getRecommendation(c.recommendations), comparingCompanies.map(cc => getRecommendation(cc.recommendations)))}>
-                            {getRecommendationText(getRecommendation(c.recommendations))}
-                        </td>)}
-                    </tr>
+                    {visibility.Recommendations &&
+                        <Fragment>
+                            <tr className='groupName'>
+                                <th colSpan={comparingCompanies.length + 1} className='centered'>Recommendations</th>
+                            </tr>
 
-                    <tr>
-                        <td>Average Price Target</td>
-                        {comparingCompanies.map((c, i) => <td key={i} className='value'>{`$${c.priceTargets.targetMean}`}</td>)}
-                    </tr>
+                            {visibility.Consensus &&
+                                <Fragment>
+                                    <tr>
+                                        <td>Consensus</td>
+                                        {comparingCompanies.map((c, i) => <td key={i}
+                                            className={getNumberClass(
+                                                getRecommendation(c.recommendations), comparingCompanies.map(cc => getRecommendation(cc.recommendations)))}>
+                                            {getRecommendationText(getRecommendation(c.recommendations))}
+                                        </td>)}
+                                    </tr>
+                                </Fragment>
+                            }
 
-                    <tr>
-                        <td>Upside to Average Price Target</td>
-                        {comparingCompanies.map((c, i) => <td key={i}
-                            className={getNumberClass(getUpside(c), comparingCompanies.map(cc => getUpside(cc)))}>
-                            {`${getUpside(c)}%`}
-                        </td>)}
-                    </tr>
+                            {visibility['Average Price Target'] &&
+                                <Fragment>
+                                    <tr>
+                                        <td>Average Price Target</td>
+                                        {comparingCompanies.map((c, i) => <td key={i} className='value'>{`$${c.priceTargets.targetMean}`}</td>)}
+                                    </tr>
+                                </Fragment>
+                            }
+
+                            {visibility['Upside to Average Price Target'] &&
+                                <Fragment>
+                                    <tr>
+                                        <td>Upside to Average Price Target</td>
+                                        {comparingCompanies.map((c, i) => <td key={i}
+                                            className={getNumberClass(getUpside(c), comparingCompanies.map(cc => getUpside(cc)))}>
+                                            {`${getUpside(c)}%`}
+                                        </td>)}
+                                    </tr>
+                                </Fragment>
+                            }
+
+                        </Fragment>
+                    }
 
                 </tbody>
             </Table>
+
+            <Modal show={showSettings} onHide={handleHideSettings}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Comparison settings</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Table bordered hover variant='light' className='table-sm'>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Visibility</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Object.keys(visibilityTmp).map((name, i) =>
+                                <tr key={i} className={isGroup(name) ? 'groupName' : ''}>
+                                    {isGroup(name) && <th>{name}</th>}
+                                    {!isGroup(name) && <td>{name}</td>}
+
+                                    <td className='centered'>
+                                        <input type='checkbox' checked={visibilityTmp[name]} onChange={(e) => handleVisibilityChanged(name, e)}>
+
+                                        </input>
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </Table>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleHideSettings}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={saveSetting}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
     )
 }
