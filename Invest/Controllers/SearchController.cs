40e235833a6712b7264ad1e58eace0a7b12a5193
morozec <andre15@inbox.ru>
2020-06-07 21:08:40 +0400

    fetch etfs from Moscow Exchange

--- Invest/Controllers/SearchController.cs
+++ Invest/Controllers/SearchController.cs
@@ -43,36 +43,52 @@ namespace Invest.Controllers
             var moexUrl =
                 "http://iss.moex.com/iss/engines/stock/markets/shares/boards/tqbr/securities.json";
 
+            
+            var moexUrlTqtf =
+                "http://iss.moex.com/iss/engines/stock/markets/shares/boards/tqtf/securities.json";
+
             var deserial = new JsonDeserializer();
 
-            var client = new RestClient(nasdaqUrl);
+            //var client = new RestClient(nasdaqUrl);
+            //var request = new RestRequest(Method.GET);
+            //IRestResponse response = client.Execute(request);
+            //var jsonNasdaqCompanies = deserial.Deserialize<List<Dictionary<string, string>>>(response);
+            //var nasdaqCompanies = jsonNasdaqCompanies.Select(
+            //    c => new Company(c["Symbol"], c["Company Name"], c["Security Name"], "NASDAQ"));
+
+
+            //client = new RestClient(nyseUrl);
+            //request = new RestRequest(Method.GET);
+            //response = client.Execute(request);
+            //var jsonNuseCompanies = deserial.Deserialize<List<Dictionary<string, string>>>(response);
+            //var nuseCompanies = jsonNuseCompanies.Select(
+            //    c => new Company(c["ACT Symbol"], c["Company Name"], c["Company Name"], "NYSE"));
+
+            //client = new RestClient(moexUrl);
+            //request = new RestRequest(Method.GET);
+            //response = client.Execute(request);
+            //var jsonMoexCompanies = deserial.Deserialize<Dictionary<string, Dictionary<string, List<List<string>>>>>(response);
+            //var moexSecurities = jsonMoexCompanies["securities"];
+            //var moexDatas = moexSecurities["data"];
+            //var moexCompanies = moexDatas.Select(
+            //    c => new Company(c[0], c[2], c[9], "MOEX"));
+
+           
+
+            var client = new RestClient(moexUrlTqtf);
             var request = new RestRequest(Method.GET);
-            IRestResponse response = client.Execute(request);
-            var jsonNasdaqCompanies = deserial.Deserialize<List<Dictionary<string, string>>>(response);
-            var nasdaqCompanies = jsonNasdaqCompanies.Select(
-                c => new Company(c["Symbol"], c["Company Name"], c["Security Name"], "NASDAQ"));
-
-
-            client = new RestClient(nyseUrl);
-            request = new RestRequest(Method.GET);
-            response = client.Execute(request);
-            var jsonNuseCompanies = deserial.Deserialize<List<Dictionary<string, string>>>(response);
-            var nuseCompanies = jsonNuseCompanies.Select(
-                c => new Company(c["ACT Symbol"], c["Company Name"], c["Company Name"], "NYSE"));
-
-            client = new RestClient(moexUrl);
-            request = new RestRequest(Method.GET);
-            response = client.Execute(request);
-            var jsonMoexCompanies = deserial.Deserialize<Dictionary<string, Dictionary<string, List<List<string>>>>>(response);
-            var moexSecurities = jsonMoexCompanies["securities"];
-            var moexDatas = moexSecurities["data"];
-            var moexCompanies = moexDatas.Select(
+            var response = client.Execute(request);
+            var jsonMoexCompaniesTqtf = deserial.Deserialize<Dictionary<string, Dictionary<string, List<List<string>>>>>(response);
+            var moexSecuritiesTqtf = jsonMoexCompaniesTqtf["securities"];
+            var moexDatasTqtf = moexSecuritiesTqtf["data"];
+            var moexCompaniesTqtf = moexDatasTqtf.Select(
                 c => new Company(c[0], c[2], c[9], "MOEX"));
 
             var allCompanies = new List<Company>();
-            allCompanies.AddRange(nasdaqCompanies);
-            allCompanies.AddRange(nuseCompanies);
-            allCompanies.AddRange(moexCompanies);
+            //allCompanies.AddRange(nasdaqCompanies);
+            //allCompanies.AddRange(nuseCompanies);
+            //allCompanies.AddRange(moexCompanies);
+            allCompanies.AddRange(moexCompaniesTqtf);
 
             _companyContext.Companies.AddRange(allCompanies);
 
